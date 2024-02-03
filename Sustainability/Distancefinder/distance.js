9d8c88790c3f2c060a6e3bf504f523f337dd3275
Zeyu Huang <zeze33629@gmail.com>
2024-02-03 16:21:38 -0500

    the rest of the stuff for the pevious commit

--- Sustainability/Distancefinder/distance.js
+++ Sustainability/Distancefinder/distance.js
@@ -1,5 +1,13 @@
 // To find distance between 2 location
 function findDistance(location1, location2) {
     let geocoder;
+    let request;
     geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode(request).then((result) => {
+        const { results } = result;
+        JSON.stringify(result, null, 2);
+    }).catch((e) => {
+        alert("Geocode was not successful for the following reason: " + e);
+    })
 }
