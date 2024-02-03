f1b4237bdb7a3de32401ce3e14e7ac296df900e2
Zeyu Huang <zeze33629@gmail.com>
2024-02-03 16:10:25 -0500

    trying to make distance detection

---
+++ Sustain.py
@@ -0,0 +1,7 @@
+import json
+from geopy.geocoders import Nominatim
+
+def parseJson(String str):
+    info = json.loads(str)
+
+def checkSustainability(String str):
---
+++ Sustainability/Distancefinder/distance.js
@@ -0,0 +1,5 @@
+// To find distance between 2 location
+function findDistance(location1, location2) {
+    let geocoder;
+    geocoder = new google.maps.Geocoder();
+}
