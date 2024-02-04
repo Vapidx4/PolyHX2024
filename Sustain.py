b206195a3feb620ed461763848b527ccc6b6658c
Gabriel Saberian/ <gabsab1000@gmail.com>
2024-02-03 16:17:53 -0800

    Revert "trying to make distance detection"
    This reverts commit f1b4237bdb7a3de32401ce3e14e7ac296df900e2.

--- Sustain.py
+++
@@ -1,7 +0,0 @@
-import json
-from geopy.geocoders import Nominatim
-
-def parseJson(String str):
-    info = json.loads(str)
-
-def checkSustainability(String str):
--- Sustainability/Distancefinder/distance.js
+++
@@ -1,5 +0,0 @@
-// To find distance between 2 location
-function findDistance(location1, location2) {
-    let geocoder;
-    geocoder = new google.maps.Geocoder();
-}
